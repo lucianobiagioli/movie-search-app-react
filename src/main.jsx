2241c1e1b12d8c1717074003aa99e9c31762991a
Lucinao <biagioli.lm@gmail.com>
2023-12-20 20:31:06 -0300

    Aplicacion de busqueda de peliculas con React JS

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BuscadorPelicula } from './BuscadorPelicula'
+import "./styles/busqueda.css"
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BuscadorPelicula />
+  </React.StrictMode>,
+)
